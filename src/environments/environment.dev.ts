8acff0cea17baf69ba15ebc6e4e30996391e7b05
ebadia <enric.badia@gmail.com>
2018-03-29 17:03:30 +0200

    fix ws ports

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,4 @@
+export const ENV = {
+  mode: 'Development',
+  ws_url: 'favolist-dev.eu-west-1.elasticbeanstalk.com'
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const ENV = {
+  mode: 'Production',
+  ws_url: 'favolist-dev.eu-west-1.elasticbeanstalk.com'
+}
