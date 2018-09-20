9320b9ccca4cec49c457bc4af097af88e007d45a
ebadia <enric.badia@gmail.com>
2018-09-20 14:07:59 +0200

    login changed to heroku

--- src/providers/config.ts
+++ src/providers/config.ts
@@ -2,5 +2,5 @@ export const config: any = {
   defaultLocale: 'us',
 
   // API links
-  API_URL: 'http://favolist-dev.eu-west-1.elasticbeanstalk.com/api/v1'
+  API_URL: 'https://favolist-db.herokuapp.com/api/v1'
 }
